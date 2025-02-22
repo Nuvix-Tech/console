2a201992549dd09204e27da1dae57b2502452731
Ravikant Saini <ravikantsaini047@gmail.com>
2025-02-21 22:37:02 -0500

    Refactor Home component; remove unused state and imports, update logo link, and enhance welcome message

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,101 +1,22 @@
 "use client";
 
 import type React from "react";
-import { useState } from "react";
-
 import {
-  Avatar,
-  AvatarGroup,
   Background,
   Button,
-  Card,
   Column,
-  type DateRange,
-  DateRangePicker,
-  Dialog,
   Fade,
-  Feedback,
   Heading,
-  HoloFx,
-  Icon,
   IconButton,
   InlineCode,
-  Input,
-  Line,
   Logo,
-  LogoCloud,
-  PasswordInput,
   Row,
-  SegmentedControl,
-  Select,
-  SmartImage,
-  SmartLink,
-  StatusIndicator,
-  StyleOverlay,
-  Switch,
-  TagInput,
   Text,
-  Textarea,
   TiltFx,
-  useToast,
 } from "@/ui/components";
-import { CodeBlock, MediaUpload } from "@/ui/modules";
+import { CodeBlock } from "@/ui/modules";
 
 export default function Home() {
-  const [selectedValue, setSelectedValue] = useState("");
-  const [selectedRange, setSelectedRange] = useState<DateRange>();
-  const [isFirstDialogOpen, setIsFirstDialogOpen] = useState(false);
-  const [isSecondDialogOpen, setIsSecondDialogOpen] = useState(false);
-  const [firstDialogHeight, setFirstDialogHeight] = useState<number>();
-  const { addToast } = useToast();
-  const [intro, setIntro] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [tags, setTags] = useState<string[]>(["UX / UI", "Design systems", "AI / ML"]);
-  const [twoFA, setTwoFA] = useState(false);
-
-  const handleSelect = (value: string) => {
-    console.log("Selected option:", value);
-    setSelectedValue(value);
-  };
-
-  const links = [
-    {
-      href: "https://once-ui.com/docs/theming",
-      title: "Themes",
-      description: "Style your app in minutes",
-    },
-    {
-      href: "https://once-ui.com/docs/RowComponent",
-      title: "Layout",
-      description: "Build responsive layouts",
-    },
-    {
-      href: "https://once-ui.com/docs/typography",
-      title: "Typography",
-      description: "Scale text automatically",
-    },
-  ];
-
-  const validateIntro = (value: React.ReactNode) => {
-    if (typeof value === "string" && value.length < 10) {
-      return (
-        <Row horizontal="center" marginBottom="12" gap="8">
-          <Icon name="errorCircle" />
-          Intro must be at least 10 characters long.
-        </Row>
-      );
-    }
-    return null;
-  };
-
-  const validateLogin = () => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regex.test(email)) {
-      return "Email and / or password is invalid.";
-    }
-    return null;
-  };
 
   return (
     <Column fillWidth paddingY="80" paddingX="s" horizontal="center" flex={1}>
@@ -125,7 +46,7 @@ export default function Home() {
           <Logo
             size="m"
             icon={false}
-            href="https://once-ui.com"
+            href="https://nuvix.in"
             wordmarkSrc="/trademark/nuvix-logo-dark.svg"
           />
           <Row gap="12" hide="s">
@@ -145,7 +66,6 @@ export default function Home() {
               weight="default"
               variant="tertiary"
             />
-            <StyleOverlay top="20" right="24" />
           </Row>
           <Row gap="16" show="s" horizontal="center" paddingRight="24">
             <IconButton href="https://discord.com/invite" icon="discord" variant="tertiary" />
@@ -154,7 +74,6 @@ export default function Home() {
               icon="github"
               variant="tertiary"
             />
-            <StyleOverlay top="20" right="24" />
           </Row>
         </Row>
       </Row>
@@ -228,14 +147,17 @@ export default function Home() {
           />
           <Column fillWidth horizontal="center" gap="32" padding="32" position="relative">
             <InlineCode radius="xl" shadow="m" fit paddingX="16" paddingY="8">
-              Start by editing{" "}
+              Currently{" "}
               <Text onBackground="brand-medium" marginLeft="8">
-                app/page.tsx
+                In Devlopment
               </Text>
             </InlineCode>
             <Heading wrap="balance" variant="display-default-l" align="center" marginBottom="16">
               Welcome to Console
             </Heading>
+            <Text>
+              Click below button to access the dashboard
+            </Text>
             <Button id="readDocs" label="Console" href="/console" variant="secondary" arrowIcon />
           </Column>
         </Column>
