6f09bcf1332a75587c784e88e64440e788721c47
ravikantsaini047 <ravikantsaini047@gmail.com>
2025-02-25 11:08:50 +0000

    Document Page Metadata Comp

--- src/components/project/database/single/collection/document/_page.tsx
+++ src/components/project/database/single/collection/document/_page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { CardBox } from "@/components/others/card";
+import { getCollectionPageState, getDbPageState, getDocumentPageState } from "@/state/page";
+import { Column } from "@/ui/components";
 import React from "react";
 
 type Props = {
@@ -7,12 +11,36 @@ type Props = {
   documentId: string;
 };
 
-const DocumentPage: React.FC<Props> = () => {
+const DocumentPage: React.FC<Props> = ({ id, databaseId, collectionId, documentId }) => {
+  const { document } = getDocumentPageState();
+  const { collection } = getCollectionPageState();
+  const { database } = getDbPageState();
+
+  if (
+    document?.$id !== documentId ||
+    collection?.$id !== collectionId ||
+    database?.$id !== databaseId
+  )
+    return;
+
   return (
-    <div>
-      <h1>Document Page</h1>
-      <p>This is the document page for a single collection in the database.</p>
-    </div>
+    <Column gap="20" padding="20" fillWidth>
+      <MetaData />
+    </Column>
+  );
+};
+
+const MetaData = () => {
+  const { document } = getDocumentPageState();
+  const { collection } = getCollectionPageState();
+  const { database } = getDbPageState();
+
+  return (
+    <CardBox>
+      <p>Document: {document?.$id}</p>
+      <p>Collection: {collection?.$id}</p>
+      <p>Database: {database?.$id}</p>
+    </CardBox>
   );
 };
 
