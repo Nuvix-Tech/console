14d3313630aa6fc68ad17ffb9ef76627c053691e
Ravikant Saini <ravikantsaini047@gmail.com>
2025-02-25 08:06:29 -0500

    fix: correct the import of the models in the document page

--- src/components/project/database/single/collection/document/_page.tsx
+++ src/components/project/database/single/collection/document/_page.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { CardBox } from "@/components/others/card";
+import { CardBox, CardBoxBody, CardBoxItem, CardBoxTitle } from "@/components/others/card";
+import { formatDate } from "@/lib/utils";
 import { getCollectionPageState, getDbPageState, getDocumentPageState } from "@/state/page";
 import { Column } from "@/ui/components";
+import { Text } from "@chakra-ui/react";
 import React from "react";
 
 type Props = {
@@ -26,6 +28,31 @@ const DocumentPage: React.FC<Props> = ({ id, databaseId, collectionId, documentI
   return (
     <Column gap="20" padding="20" fillWidth>
       <MetaData />
+      <CardBox>
+        <CardBoxBody>
+          <CardBoxItem>
+            <CardBoxTitle>
+              Document
+            </CardBoxTitle>
+          </CardBoxItem>
+          <CardBoxItem>
+            <Column gap="12">
+              <Text>
+                {collection?.name} - {database?.name}
+              </Text>
+              <Text>
+                {document?.$id}
+              </Text>
+              <Text>
+                {formatDate(document?.$createdAt)}
+              </Text>
+              <Text>
+                {formatDate(document?.$updatedAt)}
+              </Text>
+            </Column>
+          </CardBoxItem>
+        </CardBoxBody>
+      </CardBox>
     </Column>
   );
 };
@@ -37,9 +64,21 @@ const MetaData = () => {
 
   return (
     <CardBox>
-      <p>Document: {document?.$id}</p>
-      <p>Collection: {collection?.$id}</p>
-      <p>Database: {database?.$id}</p>
+      <CardBoxBody>
+        <CardBoxItem>
+          <CardBoxTitle>
+            Metadata
+          </CardBoxTitle>
+        </CardBoxItem>
+        <CardBoxItem>
+          <Text>
+            {formatDate(document?.$createdAt)}
+          </Text>
+          <Text>
+            {formatDate(document?.$updatedAt)}
+          </Text>
+        </CardBoxItem>
+      </CardBoxBody>
     </CardBox>
   );
 };
