a699d27bc41a621597883e45d4d36bdeb1252cdd
Ravikant Saini <ravikantsaini047@gmail.com>
2025-03-17 04:29:43 -0400

    Update environment variable names for API endpoint and table border configuration

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Nuvix";
 export const APP_NAME_SMALL = "Nuvix";
 export const API_ENDPOINT =
-  process.env.PUBLIC_NUVIX_ENDPOINT ?? "https://strapi.collegejaankaar.in/v1";
-export const SHOW_TABLE_BORDER = process.env.TABLE_BORDER === "true";
+  process.env.NEXT_PUBLIC_NUVIX_ENDPOINT ?? "https://api.nuvix.in/v1";
+export const SHOW_TABLE_BORDER = process.env.NEXT_PUBLIC_TABLE_BORDER === "true";
--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -25,7 +25,7 @@ import {
 } from "@nuvix/console";
 
 const API_URL =
-  process.env.PUBLIC_NUVIX_ENDPOINT ?? process.env.NUVIX_ENDPOINT ?? "https://api.nuvix.in/v1";
+  process.env.NEXT_PUBLIC_NUVIX_ENDPOINT ?? process.env.NUVIX_ENDPOINT ?? "https://api.nuvix.in/v1";
 
 const clientConsole = new Client().setEndpoint(API_URL).setProject("console");
 
