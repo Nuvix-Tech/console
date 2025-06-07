f4b507ee079cac472c2ada84edbeddc18b9f58b0
Ravikant Saini <ravikantsaini047@gmail.com>
2025-06-07 07:49:26 +0000

    ui improvements

--- packages/ui/src/components/ConfirmDialog.tsx
+++ packages/ui/src/components/ConfirmDialog.tsx
@@ -34,27 +34,33 @@ const ConfirmDialog: React.FC<ConfirmProps> = ({
       open={isOpen}
       onOpenChange={onClose}
       closeOnEscape
-      closeOnInteractOutside
+      closeOnInteractOutside={false}
       unmountOnExit
       modal
+      size={"xs"}
+      placement={"center"}
     >
       <Portal disabled={!portalled}>
         <Dialog.Backdrop />
         <Dialog.Positioner className="bg-[var(--backdrop)]" zIndex={10000}>
-          <Dialog.Content asChild={false}>
-            <Dialog.Header>
+          <Dialog.Content
+            shadow={"none"}
+            border={"1px"}
+            borderStyle={"solid"}
+            borderColor={"border"}
+          >
+            <Dialog.Header flexDirection={"column"} gap={0.5}>
               <Dialog.Title>{title}</Dialog.Title>
               {description && <Dialog.Description>{description}</Dialog.Description>}
             </Dialog.Header>
-            <Dialog.Body>
-              {node}
-            </Dialog.Body>
+            {node && <Dialog.Body>{node}</Dialog.Body>}
             <Dialog.Footer>
               <Button
                 onClick={() => {
                   handleConfirm(false);
                 }}
                 variant="secondary"
+                size="s"
                 {...button?.cancel}
               >
                 {" "}
@@ -64,6 +70,7 @@ const ConfirmDialog: React.FC<ConfirmProps> = ({
                 onClick={() => {
                   handleConfirm(true);
                 }}
+                size="s"
                 {...button?.ok}
               >
                 {" "}
