930db63d51cc2f01d09feb579b1dc27df15a0995
Ravikant Saini <ravikantsaini047@gmail.com>
2025-06-07 09:33:54 +0000

    added sql page

---
+++ apps/console/src/app/(console)/project/[id]/sql/[queryId]/page.tsx
@@ -0,0 +1,11 @@
+import { PropsWithParams } from "@/types";
+
+export default async function ({ params }: PropsWithParams<{ queryId: string, id: string }>) {
+    const { queryId, id } = await params;
+
+    return (
+        <>
+            HELLO From Query Page
+        </>
+    )
+}
