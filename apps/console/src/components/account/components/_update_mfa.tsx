6b3b5b99eb77b9f0b0de9b8f558691efdbda28bf
Ravikant Saini <ravikantsaini047@gmail.com>
2025-07-22 06:42:30 +0000

    improve mfa update component

--- apps/console/src/components/account/components/_update_mfa.tsx
+++ apps/console/src/components/account/components/_update_mfa.tsx
@@ -1,69 +1,63 @@
-import { Form, InputSwitchField, SubmitButton } from "@/components/others/forms";
 import {
-  CardBox,
-  CardBoxBody,
-  CardBoxItem,
-  CardBoxTitle,
-  CardBoxDesc,
+    CardBox,
+    CardBoxBody,
+    CardBoxItem,
+    CardBoxTitle,
+    CardBoxDesc,
 } from "@/components/others/card";
-import * as y from "yup";
-import { useToast } from "@nuvix/ui/components";
+import { Switch, useToast } from "@nuvix/ui/components";
 import { useAppStore } from "@/lib/store";
 import { sdkForConsole } from "@/lib/sdk";
-
-const schema = y.object({
-  mfa: y.boolean(),
-});
+import { useState } from "react";
 
 export const UpdateMfa = () => {
-  const { user, setUser } = useAppStore((s) => s);
-  const { addToast } = useToast();
+    const { user, setUser } = useAppStore((s) => s);
+    const { addToast } = useToast();
+    const [mfaLoading, setMfaLoading] = useState(false);
 
-  return (
-    <>
-      <Form
-        initialValues={{
-          mfa: user?.mfa,
-        }}
-        enableReinitialize
-        validationSchema={schema}
-        onSubmit={async (values) => {
-          try {
-            const res = await sdkForConsole?.account.updateMFA(values.mfa);
+    const onSubmit = async (value: boolean) => {
+        setMfaLoading(true);
+        try {
+            const res = await sdkForConsole?.account.updateMFA(value);
             addToast({
-              variant: "success",
-              message: "Mfa status has been updated successfully.",
+                variant: "success",
+                message: "Mfa status has been updated successfully.",
             });
             setUser(res);
-          } catch (e: any) {
+        } catch (e: any) {
             addToast({
-              variant: "danger",
-              message: e.message,
+                variant: "danger",
+                message: e.message,
             });
-          }
-        }}
-      >
-        <CardBox
-          actions={
-            <>
-              <SubmitButton loadingText={"Updating..."}>Update</SubmitButton>
-            </>
-          }
-        >
-          <CardBoxBody>
-            <CardBoxItem gap={"4"}>
-              <CardBoxTitle>Multi-factor authentication</CardBoxTitle>
-              <CardBoxDesc>
-                Enhance your account's security by requiring a second sign-in method.
-                <a>Learn more</a>
-              </CardBoxDesc>
-            </CardBoxItem>
-            <CardBoxItem>
-              <InputSwitchField label={"MFA"} name="mfa" />
-            </CardBoxItem>
-          </CardBoxBody>
-        </CardBox>
-      </Form>
-    </>
-  );
+        } finally {
+            setMfaLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <CardBox>
+                <CardBoxBody>
+                    <CardBoxItem gap={"4"}>
+                        <CardBoxTitle>Multi-factor authentication</CardBoxTitle>
+                        <CardBoxDesc>
+                            Enhance your account's security by requiring a second sign-in method.
+                            <br />
+                            <a>Learn more</a>
+                        </CardBoxDesc>
+                    </CardBoxItem>
+                    <CardBoxItem>
+                        <Switch
+                            label={"Multi-factor authentication"}
+                            reverse
+                            loading={mfaLoading}
+                            disabled={mfaLoading}
+                            isChecked={user.mfa}
+                            onToggle={() => onSubmit(!user.mfa)}
+                        />
+                    </CardBoxItem>
+                </CardBoxBody>
+            </CardBox>
+        </>
+    );
 };
