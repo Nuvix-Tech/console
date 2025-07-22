3d613252810dd2f77147ce0531aed4c04731ba39
Ravikant Saini <ravikantsaini047@gmail.com>
2025-07-22 03:15:27 +0000

    fixed account layout

--- apps/console/src/components/account/_layout.tsx
+++ apps/console/src/components/account/_layout.tsx
@@ -1,19 +1,15 @@
 "use client";
 import { useAppStore } from "@/lib/store";
-import { Stack, Tabs } from "@chakra-ui/react";
-import { Column, Row, Text, Button } from "@nuvix/ui/components";
-import { Fragment } from "react";
+import { Column, Row, Text, Button, SegmentedControl } from "@nuvix/ui/components";
 import { BodyWrapper } from "./_wrapper";
 
 export const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   const { user } = useAppStore((s) => s);
 
-  const tabs = [{ label: "Overview", key: "overview" }];
-
   return (
     <>
-      <Column gap="16" paddingY="16">
-        <BodyWrapper>
+      <Column gap="16" paddingY="16" background="neutral-alpha-weak">
+        <BodyWrapper className="gap-4 flex flex-col py-0">
           <Button
             size="s"
             variant="tertiary"
@@ -23,23 +19,25 @@ export const AccountLayout = ({ children }: { children: React.ReactNode }) => {
           >
             Back to dashboard
           </Button>
-          <Row fillWidth horizontal="space-between" paddingX="16">
-            <Text variant="heading-default-m">{user.name ?? "Account"}</Text>
+          <Row fillWidth horizontal="space-between">
+            <Text variant="heading-strong-l" marginLeft="16">
+              {user.name ?? "Account"}
+            </Text>
             <Button size="s" variant="secondary">
               Logout
             </Button>
           </Row>
+          <SegmentedControl
+            fillWidth={false}
+            defaultSelected="overview"
+            buttons={[
+              { value: "overview", label: "Overview" },
+              { value: "sessions", label: "Sessions" },
+              { value: "organizations", label: "Organizations" },
+            ]}
+            onToggle={(value) => console.log(value)}
+          />
         </BodyWrapper>
-        <Tabs.Root>
-          <Tabs.List>
-            {tabs.map((t, i) => (
-              <Fragment key={i}>
-                <Tabs.Trigger value={t.key}>{t.label}</Tabs.Trigger>
-                <Tabs.Indicator />
-              </Fragment>
-            ))}
-          </Tabs.List>
-        </Tabs.Root>
       </Column>
       <BodyWrapper>{children}</BodyWrapper>
     </>
