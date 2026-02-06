e4922ef6f279ebfd058c77752cf9ad24214f7bf1
Ravikant Saini <ravikantsaini047@gmail.com>
2026-02-06 12:59:04 +0530

    implemented exposed schemas manage ui

--- apps/console/src/components/project/components/_settings_schemas.tsx
+++ apps/console/src/components/project/components/_settings_schemas.tsx
@@ -5,23 +5,45 @@ import {
   CardBoxItem,
   CardBoxTitle,
 } from "@/components/others/card";
-import { Form, InputTagField } from "@/components/others/forms";
+import { Form, InputTagField, SubmitButton } from "@/components/others/forms";
+import { rootKeys } from "@/lib/keys";
+import { sdkForConsole } from "@/lib/sdk";
 import { useProjectStore } from "@/lib/store";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export const ManageExposedSchemas = () => {
   const project = useProjectStore((state) => state.project);
+  const queryClient = useQueryClient();
 
   return (
     <Form
       key={project?.$id}
       initialValues={{
-        schemas: (project as any)?.metadata?.allowedSchemas || [], // types will be fixed later
+        schemas: (project as any)?.exposedSchemas || [], // types will be fixed later
       }}
+      enableReinitialize
       onSubmit={async (values) => {
-        // Update project metadata with allowed schemas
+        try {
+          const client = sdkForConsole.client;
+          const apiPath = `/projects/${project.$id}/metadata/exposed-schemas`;
+          const uri = new URL(client.config.endpoint + apiPath);
+
+          const apiHeaders: { [header: string]: string } = {
+            "content-type": "application/json",
+          };
+
+          await client.call("put", uri, apiHeaders, values);
+          await queryClient.invalidateQueries({
+            queryKey: rootKeys.project(project.$id),
+          });
+          toast.success("Exposed schemas list updated.");
+        } catch (e: any) {
+          toast.error(e?.message || "Someting went wrong.");
+        }
       }}
     >
-      <CardBox>
+      <CardBox actions={<SubmitButton label="Update" />}>
         <CardBoxBody>
           <CardBoxItem gap={"4"}>
             <CardBoxTitle>Exposed Schemas</CardBoxTitle>
