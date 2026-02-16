a6da635bc01b9a651f4f8beeef505a316356e3cf
Ravikant Saini <ravikantsaini047@gmail.com>
2026-02-16 20:06:12 +0530

    Refactor code structure for improved readability and maintainability

--- apps/docs/src/app/api/search/route.ts
+++ apps/docs/src/app/api/search/route.ts
@@ -3,11 +3,14 @@ import { createSearchAPI } from "fumadocs-core/search/server";
 
 export const { GET } = createSearchAPI("advanced", {
   language: "english",
-  indexes: source.getPages().map((page) => ({
-    title: page.data.title,
-    description: page.data.description,
-    url: page.url,
-    id: page.url,
-    structuredData: page.data.structuredData,
-  })),
+  indexes: source
+    .getPages()
+    .filter((p) => p.data.type !== "openapi")
+    .map((page) => ({
+      title: page.data.title as string,
+      description: page.data.description,
+      url: page.url,
+      id: page.url,
+      structuredData: page.data.structuredData,
+    })),
 });
--- apps/docs/src/lib/openapi.ts
+++ apps/docs/src/lib/openapi.ts
@@ -1,5 +1,7 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
+import specs from "../../public/specs/open-api.json";
+import { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
 
 export const openapi = createOpenAPI({
-  input: ["https://api.nuvix.in/api-yaml"],
+  input: () => ({ content: specs }) as Record<string, OpenAPIV3.Document | OpenAPIV3_1.Document>,
 });
