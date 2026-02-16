fd67bd2e9f3c886839efc18232f16ec21418f5a8
Ravikant Saini <ravikantsaini047@gmail.com>
2026-02-16 21:51:51 +0530

    feat: update OpenAPI input handling and enhance build outputs in turbo.json

--- apps/docs/src/lib/openapi.ts
+++ apps/docs/src/lib/openapi.ts
@@ -1,5 +1,8 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
+import specs from "../content/openapi.bundle.json";
+import { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
 
 export const openapi = createOpenAPI({
-  input: ["src/content/openapi.bundle.json"],
+  input: () =>
+    ({ content: specs }) as unknown as Record<string, OpenAPIV3.Document | OpenAPIV3_1.Document>,
 });
